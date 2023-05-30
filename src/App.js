35b655d0566d194c40a5d49ff4053fa9e4fce36c
Phebueno <fcbgomes@gmail.com>
2023-05-29 21:51:32 -0300

    feat: create-react-app com arquitetura inicial e alguns pacotes

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      o linkr do melhor grupo de todos
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import ResetStyle from './styles/ResetStyle.js';
+import GlobalStyle from './styles/GlobalStyle.js';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ResetStyle />
+    <GlobalStyle />
+    <App />
+  </React.StrictMode>
+);
