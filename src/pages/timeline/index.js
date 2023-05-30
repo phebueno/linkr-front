0eae1da69be1bb83e41f33c87381accf4e58c568
wesleymichael <wesleymichaelpereira@hotmail.com>
2023-05-30 13:44:00 -0300

    feat: layout addPost

--- src/pages/timeline/index.js
+++ src/pages/timeline/index.js
@@ -1,9 +1,22 @@
+import styled from "styled-components";
 import Header from "../../components/Header";
+import AddPost from "./AddPost";
 
-export default function Timeline(){
+export default function Timeline() {
     return (
         <>
-          <Header/>
+            <Header />
+            <Container>
+                <AddPost />
+
+            </Container>
         </>
     )
 }
+
+const Container = styled.div`
+    height: calc(100vh);
+    background: #333333;
+    padding-top: 150px;
+    border: 1px solid red;
+`
