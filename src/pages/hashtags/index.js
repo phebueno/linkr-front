3664c1298ac79e5e324c3e8eb064868092ddf00f
Phebueno <fcbgomes@gmail.com>
2023-05-31 20:46:27 -0300

    feat: hashtags in body posts also redirect

--- src/pages/hashtags/index.js
+++ src/pages/hashtags/index.js
@@ -9,18 +9,18 @@ import api from "../../services/api.js";
 export default function Hashtags() {
   const { hashtag } = useParams();
   const [postsData, setPostsData] = useState(undefined);
-    useEffect(()=>{
-        const promise = api.getHashtagPosts(hashtag);
-        promise.then((res) => {
-            setPostsData(res.data);
-          });
-          promise.catch((err) => {
-            console.log(err.message);
-            alert(
-              "Algo deu errado no carregamento do site. Por favor, recarregue a página."
-            );
-          });
-    },[hashtag])  
+  useEffect(() => {
+    const promise = api.getHashtagPosts(hashtag);
+    promise.then((res) => {
+      setPostsData(res.data);
+    });
+    promise.catch((err) => {
+      console.log(err.message);
+      alert(
+        "Algo deu errado no carregamento do site. Por favor, recarregue a página."
+      );
+    });
+  }, [hashtag]);
 
   return (
     <PageContainer>
@@ -29,11 +29,12 @@ export default function Hashtags() {
         <Title># {hashtag}</Title>
         <MainContainer>
           <Container>
-            {postsData && postsData.map((postData) => (
-              <UserPost postData={postData} key={postData.post.id} />
-            ))}
+            {postsData &&
+              postsData.map((postData) => (
+                <UserPost postData={postData} key={postData.post.id} />
+              ))}
           </Container>
-         <Trending />
+          <Trending />
         </MainContainer>
       </main>
     </PageContainer>
@@ -42,12 +43,11 @@ export default function Hashtags() {
 
 const MainContainer = styled.div`
   display: flex;
-  align-items:flex-start;
+  align-items: flex-start;
   gap: 25px;
 `;
 
 const Container = styled.div`
-  height: calc(100vh);
   width: 50vw;
   max-width: 611px;
   margin: 0px auto;
