9894d01f544121e44d6804de756ef57c20e9ae06
Phebueno <fcbgomes@gmail.com>
2023-06-01 20:06:28 -0300

    fix: styling and working hashtags in bodyposts

---
+++ src/utils/hashtagFormatter.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const HASHTAG_FORMATTER = string => {
+    return string.split(/((?:^|\s)(?:#[a-z\d-]+))/gi).filter(Boolean).map((v,i)=>{
+      if(v.includes('#')){
+        return <StyledHashtag key={i} to={`/hashtag/${v.trim().replace("#",'')}`}>{v}</StyledHashtag>
+      }   else{
+        return v
+      }
+    })
+  };
+
+  const StyledHashtag = styled(Link)`
+  text-decoration:none;
+  font-weight:700;
+  color: #FFFFFF;
+`;
