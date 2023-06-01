4139be95aa200d9619913956cc80186479b7d68f
Ivan <ivanaires32@gmail.com>
2023-06-01 16:30:20 -0300

    fix: corrige conflito

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Hashtags from "./pages/hashtags/index.js";
 import Timeline from "./pages/timeline";
-import UserPage from "./pages/user/UserPage";
+import UserPage from "./pages/user/index.js";
 import SignIn from "./pages/login/SignIn.js";
 import SignUp from "./pages/login/SignUp.js";
 
