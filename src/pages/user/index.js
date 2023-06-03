8e02d3e7375f0e43c6efd5bbbc1486c2077b04f3
Lúcio Bruno <luciobrunop20@hotmail.com>
2023-06-02 20:40:58 -0400

    feat/ move to user page

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -23,6 +23,7 @@ export default function UserPage() {
         promise.then((res) => {
             setPostsData(res.data.posts)
             setUserData(res.data.user)
+            console.log(res.data.posts)
         })
 
         promise.catch((err) => {
